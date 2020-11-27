6f9cb079e38ef5764e0ebc317e27caae048ddc2a
AndrzejGierlak <236411@student.pwr.edu.pl>
2020-11-27 17:36:32 +0100

    Close to end backend of app, you need to do edit, improve add and delete

--- record_edit.dart
+++ record_edit.dart
@@ -2,12 +2,12 @@
 // TODO stop, dodaj określony czas(popup odkiedy do kiedy?), podaj stawkę godzinową
 import 'package:flutter/material.dart';
 
-class TaskEditionRoute extends StatelessWidget {
+class TaskRecordRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Task edition"),
+        title: Text("Single Record of Task"),
       ),
       body: Center(
         child: ElevatedButton(
