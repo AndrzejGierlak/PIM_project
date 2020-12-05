ec0fbdad5f011ca481cde60abfccd3dc1e3c0940
AndrzejGierlak <236411@student.pwr.edu.pl>
2020-12-05 19:50:20 +0100

    edit(),remove() do tasków, recordów

---
+++ record_controller.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import "record_model.dart";
+import 'task_model.dart';
+
+Future<void> saveRecords(List<Record> tasks) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString('records', jsonEncode(tasks));
+}
+
+Future<List<Record>> getRecords() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  final String recordsStr = prefs.getString("records");
+
+  List recordsList = new List<Record>();
+  if (recordsStr != null) {
+    List<dynamic> jsonArray = json.decode(recordsStr);
+
+    for (int i = 0; i < jsonArray.length; ++i) {
+      recordsList.add(Record.fromJson(jsonArray[i]));
+    }
+  }
+
+  return recordsList;
+}
+
+Future<void> addRecord(Record record) async {
+  List<Record> recordsList = await getRecords();
+  recordsList.add(record);
+  saveRecords(recordsList);
+}
+
+Future<void> removeRecord(Record record) async{
+  List<Record> recordsList = await getRecords();
+  try{
+    recordsList.removeWhere((item) => item.id == record.id);
+  }catch (e){
+    print("[Function removeRecord in record_controller] Error:\n $e");
+  }
+  saveRecords(recordsList);
+}
+
+Future<void> editRecord(Record record, DateTime startDate, TimeOfDay duration) async{
+  List<Record> recordsList = await getRecords();
+  try{
+    recordsList.singleWhere((item) => item.id == record.id).startDate= startDate;
+    recordsList.singleWhere((item) => item.id == record.id).duration= duration;
+  }catch(e){
+    print("[Function editRecord in record_controller] Error:\n $e");
+  }
+  saveRecords(recordsList);
+}
+
+Future<void> removeRecordsFromTask(Task task) async{
+  List<Record> recordsList = await getRecords();
+  try{
+    recordsList.removeWhere((item) => item.taskId == task.id);
+  }catch(e){
+    print("[Function removeRecordsFromTask in record_controller] Error:\n $e");
+  }
+  saveRecords(recordsList);
+}
---
+++ task_controller.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import "task_model.dart";
+import "record_controller.dart";
+
+Future<void> saveTasks(List<Task> tasks) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString('tasks', jsonEncode(tasks));
+}
+
+Future<List<Task>> getTasks() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  final String tasksStr = prefs.getString("tasks");
+
+  List tasksList = new List<Task>();
+  if (tasksStr != null) {
+    List<dynamic> jsonArray = json.decode(tasksStr);
+
+    for (int i = 0; i < jsonArray.length; ++i) {
+      tasksList.add(Task.fromJson(jsonArray[i]));
+    }
+  }
+
+  return tasksList;
+}
+
+Future<void> addTask(Task task) async {
+  List<Task> tasksList = await getTasks();
+  tasksList.add(task);
+  saveTasks(tasksList);
+}
+
+Future<void> removeTask(Task task) async{
+  List<Task> tasksList = await getTasks();
+  try{
+    removeRecordsFromTask(task);
+    tasksList.removeWhere((item) => item.id == task.id);
+  }catch (e){
+    print("[Function removeTask in task_controller] Error:\n $e");
+  }
+  saveTasks(tasksList);
+}
+
+Future<void> editTask(Task task, String name, int color, String salary, String description) async{
+  List<Task> tasksList = await getTasks();
+  try{
+    tasksList.singleWhere((item) => item.id == task.id).name= name;
+    tasksList.singleWhere((item) => item.id == task.id).color= color;
+    tasksList.singleWhere((item) => item.id == task.id).salary= salary;
+    tasksList.singleWhere((item) => item.id == task.id).description= description;
+  }catch(e){
+    print("[Function editTask in task_controller] Error:\n $e");
+  }
+  saveTasks(tasksList);
+}
