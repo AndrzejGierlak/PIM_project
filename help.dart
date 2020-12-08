ddf56f6ca216fa07220de4cb1b69ac0637be324c
AndrzejGierlak <72444588+AndrzejGierlak@users.noreply.github.com>
2020-12-08 16:59:10 +0100

    Final info-screen

--- help.dart
+++ help.dart
@@ -10,17 +10,36 @@ class HelpRoute extends StatelessWidget {
         "...\n";
     return Scaffold(
       appBar: AppBar(
-        title: Text("Pomoc/Informacja"),
+        title: Text("Informacje o aplikacji"),
       ),
-      body: const Center(
-          child: Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              infoString,
-              style: TextStyle(fontSize: 24),
-            ),
-          )
-
+      body: Container(
+        color: Colors.cyan[100],
+        padding: EdgeInsets.all(5.0),
+        child: Column(
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                  child: Text('Aplikacja Pracomierz',
+                      style: TextStyle(fontSize: 40))),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child:Text("Data ostatniej kompilacji: 8.12.20",
+                        style: TextStyle(fontSize: 18))),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                    child:Text(
+                        "Twórcy:\n"
+                        "Wojtek Śliwa 241296\n"
+                        "Andrzej Gierlak 236411\n",
+                        style: TextStyle(fontSize: 24))),
+              ),
+            ]
+        )
       ),
     );
   }
