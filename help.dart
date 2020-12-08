4cf3489ec16252eeb68d9a1a3e25fb9e4a7ca47c
AndrzejGierlak <72444588+AndrzejGierlak@users.noreply.github.com>
2020-12-08 17:01:00 +0100

    Final screen without unnecessary constant string

--- help.dart
+++ help.dart
@@ -4,10 +4,6 @@ import 'package:flutter/material.dart';
 class HelpRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    const infoString="Working Hours 2.0 \n"
-        "Autorzy: Wojtek Śliwa, Andrzej Gierlak\n"
-        "Więcej info...\n"
-        "...\n";
     return Scaffold(
       appBar: AppBar(
         title: Text("Informacje o aplikacji"),
